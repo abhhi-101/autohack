bcb7833aee254b505506c62c21c362810bbb1a29
abhhi <birdawade11@gmail.com>
2021-07-27 07:55:32 +0530

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -8,7 +8,7 @@ sudo apt install httprobe
 sudo apt install subjack
 
 #others
-mkdir ~/tools/;cd ~/tools/
+mkdir -p ~/tools/;cd ~/tools/
 echo "Installing nuclie..."
 git clone https://github.com/projectdiscovery/nuclei.git
 cd nuclei/v2/cmd/nuclei; sudo go build main.go; mv main.go nuclei; sudo cp nuclei /bin/
@@ -20,6 +20,5 @@ cd ~/tools/; echo "Installing Aquatone.."
 wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
 unzip aquatone_linux_amd64_1.7.0.zip; sudo cp aquatone /bin/
 
-
 cd ~/tools/; echo "Installing GitDorker..."
-git clone https://github.com/obheda12/GitDorker.git; cd GitDorker; pip install requirements.txt
+git clone https://github.com/obheda12/GitDorker.git; cd GitDorker; python3 -m pip install requirements.txt
